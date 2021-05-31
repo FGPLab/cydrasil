4597da3fb3243b255837af90aa94f6f45a399912
Daniel Roush <dwroush@asu.edu>
2021-05-31 12:11:09 -0700

    cleaned and updated to final repo

--- json-to-fasta.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open('cydrasil-v3-database.json') as json_file:
-    data = json.load(json_file)
-    for item in data:
-        print('>' + item['cyTaxID'])
-        print(item['sequence'])
